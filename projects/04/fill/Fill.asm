051a3a9153f72f5c0c068f30b0ad26b39e8cd883
konlampro94 <konlampro94@gmail.com>
2020-12-15 21:50:26 +0200

    finished Project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,109 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+    
+  
+    //get screen  base address
+    @SCREEN
+    D=A
+    // store in the addrscr variable
+    @addrscr
+    M=D
+
+    //get keyboard address
+    @KBD
+    D=A
+    // store in the addrkbd variable
+    @addrkbd
+    M=D
+
+    // store counter variable and set to 0
+    @counter
+    M=0
+
+
+    @CHECK1
+    0;JMP
+
+(WHITE)
+
+
+(L1)
+
+    @counter
+    D=M
+    // D=i
+    @addrscr
+    A=D+M // RAM[addrscr + counter] = 0
+    M=0
+
+    @8192
+    D=A
+
+    @counter
+    M=M+1  // counter = counter + 1
+    D=D-M
+    @L1
+    D;JNE
+
+    // set counter to 0
+    @counter
+    M=0
+
+
+ (CHECK1)
+    // check (loop) if keyboard is 0, else jump to BLACK
+    @addrkbd
+    A=M
+    D=M 
+    @CHECK1
+    D;JEQ
+
+    @BLACK
+    0;JMP
+    
+
+
+(BLACK)
+
+
+(L2)
+
+    @counter
+    D=M
+    // D=i
+    @addrscr
+    A=D+M // RAM[addrscr + counter] = -1
+    M=-1
+
+    @8192
+    D=A
+
+    @counter
+    M=M+1  // counter = counter + 1
+    D=D-M
+    @L2
+    D;JNE
+
+    // set counter to 0
+    @counter
+    M=0
+
+ (CHECK2)
+    // check (loop) if keyboard is not 0, else jump to WHITE
+
+    @addrkbd
+    A=M
+    D=M 
+    @CHECK2
+    D;JNE
+
+    @WHITE
+    0;JMP
+
+
+
+(END)
+
+    @END
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,40 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+    // END if RAM[1] == 0 || RAM[0] == 0
+    // For R0 times (R0 = RO - 1) 
+    // add R1 to R2
+    // JEQ R0 = 0 => END
+
+    // Initialize result(RAM[2]) to 0
+    @R2
+    M=0
+    // RAM[1] == 0? => END
+    @R1
+    D=M      
+    @END
+    D;JEQ
+    // RAM[0] == 0? => END
+    @R0
+    D=M      
+    @END
+    D;JEQ    
+    
+    (LOOP)
+    @R0
+    D=M             
+    D=D-1   // (RAM[0] = RAM[O] - 1) 
+    M=D    
+    @R1
+    D=M    // D = RAM[1]  
+    @R2       
+    M=M+D   // RAM[2] = RAM[2] + RAM[1]
+    @R0
+    D=M           
+    @LOOP
+    D;JNE  // RAM[0] != 0
+    (END)
+    @END
+    0;JMP
+
