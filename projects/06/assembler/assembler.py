22126be3d4046ef4d5aab3a0ac416c83d72999c0
l-const <const_l@outlook.com>
2020-12-19 00:39:23 +0200

    assembler test vol.2

--- projects/06/assembler/assembler.py
+++ projects/06/assembler/assembler.py
@@ -7,18 +7,11 @@ from typing import List
 from parser import Command, Parser
 from codegen import Code
 from symbol_table import SymbolTable
-from pprint import pprint
-# import Parser, Command
-# import Code
-# import Symboltable
-#import Parser 
-#import Code 
-#import Command
-#import SymbolTable
 
 
 class Assembler:
     def __init__(self, filename) -> None:
+        self.filename = filename
         self.parser = Parser(filename)
         self.code_gen = Code()
         self.sym_table = SymbolTable()
@@ -52,14 +45,15 @@ class Assembler:
                 # @sum
                 if not s_table.contains(sym):
                     s_table.add_entry(sym, self._base_symbol_addr)
-                    self._output[parser.counter] = f"{self._base_symbol_addr:016b}"
+                    self._output.append(f"{self._base_symbol_addr:016b}")
                     self._base_symbol_addr += 1
                 else:
                     addr = s_table.get_address(sym)
-                    self._output[parser.counter] = f"{addr:016b}"
+                    self._output.append(f"{addr:016b}")
             elif parser.command_type() == Command.A_COMMAND and sym.isnumeric():
                 # @100
-                self._output[parser.counter] = f"{sym:016b}"
+                # print(parser.counter)
+                self._output.append(f"{int(sym):016b}")
             else:
                 # C command
                 jump_asm = parser.jump()
@@ -68,20 +62,22 @@ class Assembler:
                 jump_hack = self.code_gen.jump(jump_asm)
                 dest_hack = self.code_gen.dest(dest_asm)
                 comp_hack = self.code_gen.comp(comp_asm)
-                self._output[parser.counter] = "111" + comp_hack + dest_hack + jump_hack
+                self._output.append("111" + comp_hack + dest_hack + jump_hack)
 
             parser.advance()
 
-    def code_gen(self):
+    def gen(self):
         self.name = self.filename.split(".")[0]
         self.out_file = self.name + ".hack"
         with open(self.out_file, "w+") as out_f:
-            out_f.writelines(self._output)
+            out_f.writelines(
+                ["\n" + l if p != 0 else l for p, l in enumerate(self._output)]
+            )
 
     def run(self):
         self.first_pass()
         self.second_pass()
-        self.code_gen()
+        self.gen()
 
 
 def main():
@@ -90,27 +86,30 @@ def main():
     """
     if len(sys.argv) > 1:
         assembler = Assembler(sys.argv[1])
-        # assembler.run()
+        assembler.run()
+    
     else:
         print("Please provide a filepath! -> $ python assembler.py [filepath]")
 
-
-def test_first_pass():
+def test_until_first_pass():
     """
     Test for the First pass.
     """
 
     ass = Assembler(sys.argv[1])
     ass.first_pass()
-    print(str(ass.parser)+"\n")
+    print(str(ass.parser) + "\n")
     print(ass.sym_table)
+    return ass
 
-def test_second_pass():
+def test_until_second_pass():
     """
     Test for the second pass.
     """
+    ass = test_until_first_pass()
+    ass.second_pass()
+    print(ass._output)
 
 
 if __name__ == "__main__":
-    test_first_pass()
-    #test_first_pass()
+    main()
--- projects/06/assembler/symbol_table.py
+++ projects/06/assembler/symbol_table.py
@@ -31,7 +31,7 @@ class SymbolTable:
             "R13": 13,
             "R14": 14,
             "R15": 15,
-            "SCREEN": 16834,
+            "SCREEN": 16384,
             "KBD": 24576,
             "SP": 0,
             "LCL": 1,
