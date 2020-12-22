3cd09a72e70d972261a4201a4f5431ccedec97ec
konlampro94 <konlampro94@gmail.com>
2020-12-22 10:59:40 +0200

    vm_translator vol.2

--- projects/07/vmtranslator/codewriter.py
+++ projects/07/vmtranslator/codewriter.py
@@ -5,18 +5,37 @@ class CodeWriter:
     def __init__(self, source: str) -> None:
         super().__init__()
         self.source = source
-        self.is_dir = os.path.is_dir(self.source)
+        self.is_dir = os.path.isdir(self.source)
         self._output = []
         self.counter = 0
 
     def set_filename(self, name: str):
         pass
 
-    def write_push_pop(self, cmd: str):
-        pass
+    def write_push_pop(self, cmd: str,  segment: str, index: int):
+        out = []
+        out.append(f"//{cmd} {segment} {index}")
+        self._output.append(out.pop())
+        if segment == "constant" and cmd == 2:
+            out.append(f"@{index}\nD=M")
+            out.append(f"@SP\nA=M")
+            out.append(f"M=D")
+            out.append(f"@SP\nM=M+1")
+            
+            [self._output.append(o) for o in out]
+        if cmd == 3:
+            out.append(f"@SP\nA=M")
+            out.append(f"D=M")
+            out.append(f"@SP\nM=M-1")
+            
+            [self._output.append(o) for o in out]
 
-    def write_arithmetic(self, cmd: int, segment: str, index: int):
-        pass
+    def write_arithmetic(self, cmd: int):
+        out = []
+        out.append(f"//{cmd}")
+        [self._output.append(o) for o in out]
+         
+        self.write_push_pop("pop", "", 0)
 
     def close(self):
         if not self.is_dir:
--- projects/07/vmtranslator/main.py
+++ projects/07/vmtranslator/main.py
@@ -1,5 +1,6 @@
 import sys, os
 from parser import Parser
+from codewriter import CodeWriter
 
 
 def main():
@@ -11,11 +12,37 @@ def main():
             vm_files = [(source + "/" + f) for f in os.listdir(source)]
             parsers = [Parser(f) for f in vm_files]
             # [print(len(p.lines)) for p in parsers]
+            run(parsers, 0)
         else:
             print("path is file")
-
+            p = Parser(source)
+            run(p, 1)
     else:
         print("Please specify filename: => python3 parser.py [filename] ")
+
+
+def run(parsers, mode):
+    if mode == 0:
+        c_writer = CodeWriter(parsers[0].filename)
+        for p in parsers:
+            while p.has_more_commands():
+                cmd_type = p.command_type()
+                if cmd_type == 2 or cmd_type == 3:
+                    c_writer.write_push_pop(p._cur()[0], p.arg1(), p.arg2())
+                else:
+                    c_writer.write_arithmetic(p._cur()[0])
+                p.advance()
+    else:
+        p = parsers
+        c_writer = CodeWriter(p.filename)
+        while p.has_more_commands():
+            cmd_type = p.command_type()
+            if cmd_type == 2 or cmd_type == 3:
+                c_writer.write_push_pop(p._cur()[0], p.arg1(), p.arg2())
+            else:
+                c_writer.write_arithmetic(p._cur()[0])
+            p.advance()
+    c_writer.close()
 
 
 if __name__ == "__main__":
