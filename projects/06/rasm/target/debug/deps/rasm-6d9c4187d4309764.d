bbec5a821a3609731efd7410b128e9acb6876d99
konlampro94 <konlampro94@gmail.com>
2020-12-22 02:51:11 +0200

    remove target/

--- projects/06/rasm/target/debug/deps/rasm-6d9c4187d4309764.d
+++
@@ -1,8 +0,0 @@
-/home/kon/Desktop/nand2tetris/projects/06/rasm/target/debug/deps/rasm-6d9c4187d4309764.rmeta: src/main.rs src/code_gen.rs src/parser.rs src/symbol.rs
-
-/home/kon/Desktop/nand2tetris/projects/06/rasm/target/debug/deps/rasm-6d9c4187d4309764.d: src/main.rs src/code_gen.rs src/parser.rs src/symbol.rs
-
-src/main.rs:
-src/code_gen.rs:
-src/parser.rs:
-src/symbol.rs:
